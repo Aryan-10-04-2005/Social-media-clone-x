28bd4a690291226d719a9a5b8c8548f3fb2de951
Aryan <155870031+Aryan-10-04-2005@users.noreply.github.com>
2026-02-27 18:08:47 +0530

    Commit
    the simple rule for where you use export and import in JavaScript

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-import {tweetsData} from './data.js'
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
-console.log(tweetsData)
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
+
+function getFeedHtml() {
+    let feedHtml = ""
+
+    for (const tweet of tweetsData ) {
+        feedHtml += `
+            <div class="tweet">
+                <div class="tweet-inner">
+                    <img src="${tweet.profilePic}" class="profile-pic">
+                    <div>
+                        <p class="handle">${tweet.handle}</p>
+                        <p class="tweet-text">${tweet.tweetText}</p>
+                        <div class="tweet-details">
+                            <span class="tweet-detail">
+                                ${tweet.replies.length}
+                            </span>
+                            <span class="tweet-detail">
+                                ${tweet.likes}
+                            </span>
+                            <span class="tweet-detail">
+                                ${tweet.retweets}
+                            </span>
+                        </div>   
+                    </div>            
+                </div>
+            </div>
+        `
+    }
+
+    console.log(feedHtml)
+    return feedHtml
+}
+
+getFeedHtml()
+
+
