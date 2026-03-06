f710c1642082b4df84ef6d83898bd73a4ce0769d
Aryan <155870031+Aryan-10-04-2005@users.noreply.github.com>
2026-03-06 20:43:58 +0530

    like  a tweet

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+
+})
+
+function handleLikeClick(tweetId){
+    console.log(tweetId)
+
+}
+
 function getFeedHtml(){
     let feedHtml = ``
-
     
     tweetsData.forEach(function(tweet){
         feedHtml += `
