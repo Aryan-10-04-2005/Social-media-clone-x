8ce02b4026a2fd7bcd08eb687f5179642e881bb1
Aryan <155870031+Aryan-10-04-2005@users.noreply.github.com>
2026-03-08 22:41:24 +0530

    showing  the  reply

--- index.js
+++ index.js
@@ -62,7 +62,25 @@ function getFeedHtml(){
             retweetIconClass = 'retweeted'
         }
         
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+            })
 
+        }
+        
           
         feedHtml += `
 <div class="tweet">
@@ -93,8 +111,14 @@ function getFeedHtml(){
             </div>   
         </div>            
     </div>
+    <div id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
 </div>
 `
+
+
+
    })
    return feedHtml 
 }
